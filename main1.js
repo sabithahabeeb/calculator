4546f89406e9fbe71f3d35aeb01b1695c4e02db0
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-08-21 15:33:21 +0530

    calculator

---
+++ main1.js
@@ -0,0 +1,20 @@
+// display btn content in input box
+function display(content){
+    result.value +=content
+}
+
+function resetResult(){
+    result.value=""
+}
+
+function exprEval(){
+    try{
+        result.value=eval(result.value)
+    }catch{
+        result.value="Invalid expression!!!"
+    }
+}
+
+function removeLast(){
+    result.value = result.value.slice(0,-1)
+}
